ae88ba2425e69596329c6e311e8d7d488120f21b
Morty <yiweichi1@gmail.com>
2026-05-18 15:24:41 +0800

    feat: extend PMU counter events

--- src/perf_counter.cpp
+++ src/perf_counter.cpp
@@ -27,16 +27,93 @@ PerfCounter::~PerfCounter() {
 }
 
 void PerfCounter::init(PerfEvent event) {
-    config_ = static_cast<uint64_t>(event);
-
     perf_event_attr attr{};
-    attr.type           = PERF_TYPE_HARDWARE;
     attr.size           = sizeof(perf_event_attr);
-    attr.config         = config_;
     attr.disabled       = 1;
     attr.exclude_kernel = 1;
     attr.exclude_hv     = 1;
     attr.inherit        = 0;
+
+    switch (event) {
+        case PerfEvent::CpuCycles:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_CPU_CYCLES;
+            break;
+        case PerfEvent::Instructions:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_INSTRUCTIONS;
+            break;
+        case PerfEvent::CacheReferences:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_CACHE_REFERENCES;
+            break;
+        case PerfEvent::CacheMisses:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_CACHE_MISSES;
+            break;
+        case PerfEvent::Branches:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_BRANCH_INSTRUCTIONS;
+            break;
+        case PerfEvent::BranchMisses:
+            attr.type   = PERF_TYPE_HARDWARE;
+            attr.config = PERF_COUNT_HW_BRANCH_MISSES;
+            break;
+        case PerfEvent::PageFaults:
+            attr.type   = PERF_TYPE_SOFTWARE;
+            attr.config = PERF_COUNT_SW_PAGE_FAULTS;
+            break;
+        case PerfEvent::L1DReferences:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_L1D |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
+            break;
+        case PerfEvent::L1DMisses:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_L1D |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+            break;
+        case PerfEvent::L1IReferences:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_L1I |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
+            break;
+        case PerfEvent::L1IMisses:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_L1I |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+            break;
+        case PerfEvent::L2References:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_LL |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
+            break;
+        case PerfEvent::L2Misses:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_LL |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+            break;
+        case PerfEvent::L3References:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_LL |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
+            break;
+        case PerfEvent::L3Misses:
+            attr.type   = PERF_TYPE_HW_CACHE;
+            attr.config = PERF_COUNT_HW_CACHE_LL |
+                          (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                          (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+            break;
+    }
+
+    config_ = attr.config;
 
     fd_ = static_cast<int>(perf_event_open(&attr, 0, -1, -1, 0));
     if (fd_ == -1) {
